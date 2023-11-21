27c3817dbd7f7d88b232a20816b1e0f35d8fd5c7
filbertmarina <112921911+filbertmarina@users.noreply.github.com>
2023-11-22 00:06:06 +0700

    Add files via upload

---
+++ ex05/sensor1/sensor1/sensor1_code.py
@@ -0,0 +1,61 @@
+import rclpy, sys
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan, Image
+from cv_bridge import CvBridge
+import cv2
+import numpy as np
+
+
+#a = 0.2
+#flag = 0
+
+is_obstacle=0
+
+class CirclePublisher(Node):
+
+    def __init__(self):
+        super().__init__('publisher')
+        self.publisher = self.create_publisher(Twist, '/robot/cmd_vel', 5) 
+        self.subscription = self.create_subscription(Image, "/depth/image", self.listener_callback, 10)
+        self.timer = self.create_timer(0.5, self.timer_callback)
+
+    def timer_callback(self):
+        twist = Twist()
+        if is_obstacle:
+            twist.linear.x = 0.
+        else:
+            twist.linear.x = 1.
+        self.publisher.publish(twist)
+    
+    
+    
+
+    def listener_callback(self, msg):     
+        cv_bridge = CvBridge()
+        depth_image = cv_bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+        depth_array = np.array(depth_image, dtype=np.float32)
+        h, w = depth_array.shape
+        global is_obstacle
+        if depth_array[h//2][w//2] < 1:
+            is_obstacle = 1
+        else:
+            is_obstacle = 0
+        	
+
+        
+        
+def main(args=None):
+    rclpy.init(args=args)
+
+    circling = CirclePublisher()
+
+    rclpy.spin(circling)
+
+    #circling.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+    
